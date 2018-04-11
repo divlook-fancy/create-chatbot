ba91252caa9d787ba56fbf62b4fcdb80c074f03d
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-04-11 11:15:01 +0900

    설정 파일

---
+++ pm2.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+module.exports = {
+  apps: [
+    {
+      name: 'chatbot-kakao',
+      script: 'bin/www',
+      cwd: './',
+      env: {
+        NODE_ENV: 'production', // development = 개발용, production = 서비스
+        PORT: 56003,
+      },
+      watch: true,
+      log_file: path.resolve(__dirname, './log/pi.log'),
+      out_file: path.resolve(__dirname, './log/pi.log'),
+      error_file: path.resolve(__dirname, './log/pi.log'),
+      merge_logs: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+      ignore_watch: ['log'],
+    },
+  ],
+}
