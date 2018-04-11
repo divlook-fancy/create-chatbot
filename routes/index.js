b7e93f52626d477cfbbef3fdc8040d3311bc9315
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-04-11 10:12:52 +0900

    url 카카오만 사용

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-router.use('/kakao', require('./api/kakao'))
+router.use('/', require('./api/kakao'))
 
 module.exports = router
