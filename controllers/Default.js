a6c5b7a97480875224b8f3f88f67c6c778545057
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-04-24 05:26:30 +0900

    문구수정

--- controllers/Default.js
+++ controllers/Default.js
@@ -3,7 +3,7 @@
  */
 const chatStart = ({ req, res, next }, { param, result }) => {
   let randomText = [
-    '나는야 월루~~.\n너도 월루?!',
+    '곧 날씨 기능이 생긴데\n|ㅅㅇ\n|ㅇ',
     '응???',
     '아직 할 줄 아는게 많이 없어',
     '곧 새로운 기능이 생길거야\n기대하라구!!',
