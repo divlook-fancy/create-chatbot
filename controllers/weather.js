8ce2cf5f62e5a57a76bb42a0471e47545cfaa34f
UIHYEON.CHA.OSX <oliver56@naver.com>
2018-04-23 21:36:30 +0900

    날씨

---
+++ controllers/weather.js
@@ -0,0 +1,19 @@
+'use stric'
+/**
+ * @author uihyeon<uihyeon.cha@gmail.com>
+ * @description
+ * - API 참고 : http://api.wunderground.com/api/{{Api-Key}}/{{Forecast}}/q/{{Query}}.json
+ * - example : http://api.wunderground.com/api/{{Api-Key}}/forecast/q/ko/jeonju.json
+ * - example : http://api.wunderground.com/api/{{Api-Key}}/forecast/q/autoip.json
+ * @see https://www.wunderground.com/weather/api
+ * @see Query https://www.wunderground.com/weather/api/d/docs?d=data/index
+ * @see CityName https://www.wunderground.com/global/KO.html
+ */
+class Weather {
+  /**
+   * 날씨
+   */
+  constructor() {
+
+  }
+}
